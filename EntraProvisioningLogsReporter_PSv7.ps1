86f65ec9ed06a3ba944f3b6da7a6d268f9ab1d29
FIMTooler <30675401+FIMTooler@users.noreply.github.com>
2025-09-22 20:19:21 -0400

    Add Support for activityDateTime as DateTime
    Add Support for activityDateTime as DateTime

--- EntraProvisioningLogsReporter_PSv7.ps1
+++ EntraProvisioningLogsReporter_PSv7.ps1
@@ -135,7 +135,14 @@ for ($i = 0; $i -lt $logObj.Count; $i++)
         # prep hashTable with known fields on all objects
         $updateHT = [System.Collections.Hashtable]::new(200)
         $updateHT.Add("id", $logObj[$i]['id'])
-        $updateHT.Add("activityDateTime", $logObj[$i]['activityDateTime'].ToString().Replace('T', ' ').Replace('Z',''))
+        if ($logObj[$i]['activityDateTime'] -is [DateTime])
+        {
+            $updateHT.Add("activityDateTime", $logObj[$i]['activityDateTime'].ToString("u"))
+        }
+        else
+        {
+            $updateHT.Add("activityDateTime", $logObj[$i]['activityDateTime'].ToString().Replace('T', ' ').Replace('Z',''))
+        }
         $updateHT.Add("durationInMilliseconds", $logObj[$i]['durationInMilliseconds'])
         $updateHT.Add("tenantId", $logObj[$i]['tenantId'])
         $updateHT.Add("jobId", $logObj[$i]['jobId'])
@@ -340,3 +347,4 @@ else
 }
 
 [System.GC]::Collect()
+
